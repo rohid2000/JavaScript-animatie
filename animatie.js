3957816a597b5ee0169720b59860d6ab790bcc2f
rohid2000 <47354759+rohid2000@users.noreply.github.com>
2019-09-05 12:52:56 +0200

    Add files via upload

---
+++ animatie.js
@@ -0,0 +1,10 @@
+document.getElementById("circle").style.display = "none";
+
+function changeNow() {
+    var element = document.getElementById("animation");
+    document.getElementById("animation").style.display = "none";
+    document.getElementById("circle").style.display = "block";
+
+    circle.style.transform = "translateY(100px)";
+
+}
